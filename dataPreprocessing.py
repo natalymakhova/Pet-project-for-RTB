6d246458956889d0ebb9500a0b698920da70503b
natalymakhova <88830168+natalymakhova@users.noreply.github.com>
2022-06-23 17:19:30 +0300

    Add files via upload

--- dataPreprocessing.py
+++ dataPreprocessing.py
@@ -2,11 +2,11 @@ import pandas as pd
 import numpy as np
 from datetime import datetime, date, timedelta
 
-# Функция для определения дня недели по значению dateTime
+# Function to determine the day of the week by value dateTime
 def dateConvert(date_to_convert):
      return datetime.strptime(date_to_convert, "%Y-%m-%d %H:%M:%S").weekday()
 
-# Функция для определения часа по значению dateTime
+# Function to determine the hour by value dateTime
 def hourConvert(date_to_convert):
      return datetime.strptime(date_to_convert, "%Y-%m-%d %H:%M:%S").time().hour
 
@@ -15,11 +15,12 @@ def prepData(dataset_fragm):
     dataset_fragm['weekday'] = dataset_fragm.dateTime.apply(dateConvert)
     dataset_fragm['hour'] = dataset_fragm.dateTime.apply(hourConvert)
     dataset_fragm = dataset_fragm.drop(drop_columns, axis=1)
+    dataset_fragm[columns] = dataset_fragm[columns].astype(object, errors = 'raise')
     return dataset_fragm
 
 def  makeCutData(dataset_fragm, dataset_cut):
-        # Создание урезанной версии обучающей выборки с более равномерным
-        # распределением данных по доходам
+        # Create a stripped-down version of the training sample with a more uniform
+         # distribution of income data
         data_rev_1 = dataset_fragm.loc[dataset_fragm['revenue'] != 0]
         data_rev_0 = dataset_fragm.loc[dataset_fragm['revenue'] == 0]
         size_rev_data = len(data_rev_1.index)
@@ -33,36 +34,41 @@ def makeClassData(dataset):
     dataset_class['revenue'] = dataset_class['revenue'].map(int)
     return dataset_class
 
+columns = ['bannerSize', 'language', 'country', 'city', 'browser',
+       'browserVersion', 'os', 'osVersion', 'deviceType', 'deviceModel',
+       'deviceBrand', 'trafficSource', 'offer', 'connType', 'proxyType',
+       'weekday', 'hour']
 # Рассматриваемые даты
-start_date = date(2021, 5, 15)
+start_date = date(2021, 4, 1)
 end_date = date(2021, 6, 15)
 proc_date = start_date
 delta = timedelta(days=1)
 
 drop_columns = ['dateTime', 'campaign', 'campaignType', 'spot', 'banner', 'uid',
-    'ip', 'affiliateNetwork', 'path', 'ua', 'action', 'convType', 'convPayout', 'isp']
+    'ip', 'affiliateNetwork', 'path', 'ua', 'action', 'convType', 'convPayout',
+    'isp']
 
 dataset = pd.DataFrame()
 dataset_cut = pd.DataFrame()
 dataset_fragm = pd.DataFrame()
 
-# Цикл обработки файлов по датам
+# File processing cycle by date
 while proc_date <= end_date:
     proc_date_str = proc_date.strftime('%Y-%m-%d')
     dataset_fragm = pd.read_csv('E:/Work/RTB/forGit/data/revenue_'+
         proc_date_str + '.csv')
-    # Замена NaN нулями, добавление и удаление колонок
+    # Replacing NaN with zeros, adding and removing columns
     dataset_fragm = prepData(dataset_fragm)
 
     dataset = pd.concat([dataset, dataset_fragm])
     dataset_cut =  makeCutData(dataset_fragm, dataset_cut)
     proc_date += delta
 
-# Датасеты для решения задачи классфикации
+# Datasets for solving the classification problem
 dataset_class = makeClassData(dataset)
 dataset_cut_class = makeClassData(dataset_cut)
 
-# Сохранение данных в csv-файлы
+# Saving data to csv files
 dataset_cut.to_csv('dataset_cut.csv', index=False)
 dataset.to_csv('dataset.csv', index=False)
 dataset_cut_class.to_csv('dataset_cut_class.csv', index=False)
