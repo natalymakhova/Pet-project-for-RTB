63ec68b29878dc33b087747d4df93773db77b01f
natalymakhova <88830168+natalymakhova@users.noreply.github.com>
2022-06-23 17:16:22 +0300

    Add files via upload

--- getData.py
+++ getData.py
@@ -43,49 +43,48 @@ def concatClcFragments(df_clicks):
     return for_revenue_file, click_main
 
 
-# Переменные
-chunk_size = 10000         # Размер обрабатываемых фрагментов
-impressions_folder = '3'   # Имя папки, содержащей показы
-click_folder = '4'         # Имя папки, содержащей клики
+chunk_size = 10000         # The size of the processed fragments
+impressions_folder = '3'   # The name of the folder containing the impressions
+click_folder = '4'         # The name of the folder containing the clicks
 df_main = pd.DataFrame()
-gap = 10000000             # Величина фрагмента, который обрабатываем между выводами
-                            # информации на экран
-# Имена колонок, тип данных которых необходимо изменить
+gap = 10000000             # The size of the fragment that is processed between
+                            # displaying information on the screen
+# Column names whose data type needs to be changed
 columns = ['date', 'dateTime', 'campaign', 'campaignType', 'spot', 'banner',
        'bannerSize', 'uid', 'language', 'ip', 'country', 'city', 'browser',
        'browserVersion', 'os', 'osVersion', 'deviceType', 'deviceModel',
        'deviceBrand', 'trafficSource', 'affiliateNetwork', 'offer', 'path',
        'ua', 'connType', 'proxyType']
-# Имена колонок, по значениям которых производится объединение данных из файлов
-# с импрессиями и кликами
+# The names of the columns, according to the values of which the data from the
+# files are combined with impressions and clicks
 columns_cut = ['dateTime', 'uid', 'language', 'ip', 'country', 'city', 'browser',
     'os', 'deviceType']
 
-# Ввод данных для сервера
+# Entering data for the server
 ftp_server = input('Enter Host: ')
 username = input('Enter Username:')
 passv = input('Enter Password: ')
-# Директория для сохряняемых файлов
+# Directory for saved files
 save_dir = input('Save directory: ')
 
-# Рассматриваемые даты
+# Processed dates
 start_date = date(2021, 5, 15)
-end_date = date(2021, 6, 15)
+end_date = date(2021, 5, 15)
 proc_date = start_date
 delta = timedelta(days=1)
 
 os.chdir(save_dir)
 
-# Цикл, осуществляющий поочередную обработку файлов с импрессиями и кликами
-# за каждую дату из заданного промежутка
+# A loop that processes files with impressions and clicks one by one
+# for each date from the given interval
 while proc_date <= end_date:
     start_time = time.time()    # Начало отсчета времени
     proc_date_str, file_name_7z, file_name_clc, file_name_imp = filesName(proc_date)
 
-    # Соединение с FTP-сервером
+    # Connecting to an FTP server
     ftp = ftplib.FTP(ftp_server)
     ftp.login(username, passv)
-#  Скачивание файлов с  данными
+#  Downloading data files
     print("---Download file", file_name_7z, "from click folder")
     downloadFile(file_name_7z, click_folder, file_name_clc)
 
@@ -93,10 +92,10 @@ while proc_date <= end_date:
     downloadFile(file_name_7z, impressions_folder, file_name_imp)
 
     print('---Processing ', proc_date_str)
-    # Загрузка и обработка файла с кликами
+    # Loading and processing a file with clicks
     df_clicks = firstProcessing(file_name_clc)
 
-    # Набор случайных данных из подвыборок с ревеню и без
+    # A set of random data from subsamples with and without rhubarb
     for_revenue_file, click_main = concatClcFragments(df_clicks)
     for_revenue_file.to_csv('revenue_' + proc_date_str + '.csv', index=False)
 
@@ -104,18 +103,19 @@ while proc_date <= end_date:
     impressions_data = pd.DataFrame()
     count_chunks = 0
 
-    # Пошаговая обработка файла с импрессиями
+    # Step-by-step processing of a file with impressions
     for chunk in pd.read_csv(file_name_imp, usecols=columns, chunksize=chunk_size):
         chunk[columns] = chunk[columns].astype(object, errors = 'raise')
         chunk = chunk.drop(['date', 'affiliateNetwork'], axis=1)
 
-        # Выбор случайной строки из фрагмента с импрессиями и добавление ее в выборку
+        # Selecting a random string from a fragment with impressions and adding
+        # it to the selection
         random_samples = chunk.sample(n = 50)
         check_if_contains = random_samples.loc[~((random_samples['dateTime'].isin(df_clicks['dateTime']))
             & (random_samples['ip'].isin(df_clicks['ip'])))]
         impressions_data = impressions_data.append(check_if_contains.iloc[0:1,:])
-        # Объединение пересекающихся данных из фрагмента с импрессиями и набранных значений
-        # из выборки с кликами
+        # Combining overlapping data from a fragment with impressions and typed values
+        # from selection with clicks
         contained_rec = chunk.merge(click_main, on = columns_cut, how='inner')
 
         if not contained_rec.empty:
@@ -126,7 +126,7 @@ while proc_date <= end_date:
 
         count_chunks += 1
 
-    #  Сохранение сформированных файлов с данными
+    #  Saving generated data files
     finded_records_df.to_csv('click_data_'+proc_date_str +'.csv', index=False)
     impressions_data.to_csv('impressions_data_'+proc_date_str +'.csv', index=False)
 
@@ -134,7 +134,7 @@ while proc_date <= end_date:
     os.remove(file_name_imp)
 
     proc_date += delta
-    # Время, которое потребовалось на обработку данных, относящихся к рассматриваемой дате
+    # The time it took to process the data relating to the date in question
     print("--- %s min ---" % (np.around((time.time() - start_time)/60, decimals=2)))
     print(" -----------------------------")
 
